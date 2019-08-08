cef7c3d2f8689bc7c7a77aa2786d3abe599fe97a
Eduardo Lúcio Amorim Costa <eduardolucioac@gmail.com>
2019-08-08 18:45:50 -0300

    Update vgm.bash

--- vgm.bash
+++ vgm.bash
@@ -5,6 +5,11 @@ SCRIPTDIR_V="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $SCRIPTDIR_V/ez_i.bash
 
 read -d '' INSTRUCT_F <<"EOF"
+
+V isual
+G it
+M erge
+
 Makes the merge of a branch with the "master" branch - will OVERWRITING "master" branch - using a visual diff and merge tool (meld).
 
 TIP: TO CANCEL this process at any time use Ctrl+c!
@@ -12,7 +17,6 @@ EOF
 
 # NOTE: Mensagem de "commit" de merge do branch "master" com o branch "não master". By Questor
 MASTER_TO_NOT_M='Merge do branch \"master\" no branch \"$NOT_MASTER_BRANCH\". By Questor'
-# NOT_M_TO_MASTER='Merge do branch \"$NOT_MASTER_BRANCH\" no branch \"master\". By Questor'
 
 f_instruct "$INSTRUCT_F"
 echo ""
@@ -199,7 +203,7 @@ Handle these modifications?"
             git reset --hard origin/master
 
             f_div_section
-            echo "TIP: Your changes o \"master\" branch will be on the LEFT!
+            echo "TIP: Your changes on \"master\" branch will be on the LEFT!
 WARNING: CHANGES ON LEFT WILL BE IGNORED!"
             f_div_section
 
@@ -212,7 +216,12 @@ WARNING: CHANGES ON LEFT WILL BE IGNORED!"
             f_get_usr_input "Inform your commit message
 (all changes will be added)"
             git add --all
-            COMMIT_MSG=$GET_USR_INPUT_R
+
+            # NOTE: Evita problemas com aspas duplas dentro da "string" da mensagem.
+            # By Questor
+            f_ez_sed '"' '\"' "" 0 0 1 0 -1 "$GET_USR_INPUT_R"
+
+            COMMIT_MSG=$F_EZ_SED_R
             f_get_stderr_stdout "git commit -a -m \"$COMMIT_MSG\""
             if [[ $F_GET_STOUTERR == *"fatal: "* ]] || [[ $F_GET_STOUTERR == *"error: "* ]] ; then
                 f_enter_to_cont "$F_GET_STOUTERR"
@@ -257,7 +266,8 @@ f_branches_list() {
     TOTAL_0=${#F_SPLIT_R_0[*]}
     f=0
     for (( i=0; i<=$(( $TOTAL_0 -1 )); i++ )) ; do
-        if [[ "${F_SPLIT_R_0[$i]}" == *"refs/heads/"* ]] ; then
+        # if [[ "${F_SPLIT_R_0[$i]}" == *"refs/heads/"* ]] ; then
+        if [[ "${F_SPLIT_R_0[$i]}" == *"refs/heads/"* ]] && [[ "${F_SPLIT_R_0[$i]}" != *"refs/heads/master"* ]] ; then
             f_split "${F_SPLIT_R_0[$i]}" "refs/heads/"
             BRANCHES_ARR+=("${F_SPLIT_R[1]}")
             # https://stackoverflow.com/a/1951523/3223785
@@ -266,18 +276,94 @@ f_branches_list() {
             f=$[$f+1]
         fi
     done
+    BRANCHES_OPT_ARR+=("$f")
+    BRANCHES_OPT_ARR+=("exit")
 }
 
+# NOTE: Método de controle do processo de "merge". By Questor
+f_branches_merge_circuit() {
+    while [ ${BM_CIRCUIT_CTRL} -ge 1 ] ; do
+    # http://tldp.org/LDP/abs/html/comparison-ops.html
+
+        # >>>>> checkout "not_master"
+        f_choose_not_master_branch
+
+        f_master_to_not_master_diffs
+
+        f_merge_master_n_not_master
+
+        # >>>>> checkout "master"
+        f_merge_not_master_n_master
+
+        f_delete_merged_not_master
+
+        f_git_push_changes "master"
+
+        f_repo_backup 0
+
+        A_LENGTH=${#BRANCHES_OPT_ARR[*]}
+        if [ ${A_LENGTH} -le 2 ] ; then
+            break
+        fi
+
+        f_div_section
+        f_yes_no "Merge another branch?"
+        if [ ${YES_NO_R} -eq 1 ] ; then
+            BM_CIRCUIT_CTRL=2
+            f_repo_backup 1
+            # rm_vgm_aux_folders
+        else
+            BM_CIRCUIT_CTRL=0
+        fi
+
+    done
+
+    # NOTE: Remove arquivo que contêm as credenciais de usuário. By Questor
+    rm -f "$USR_CREDENT_FL"
+
+}
+
+# NOTE: Nome do "branch não master". By Questor
 NOT_MASTER_BRANCH=""
+
+# NOTE: Variável que controla as ações do "loop de merge". By Questor
+BM_CIRCUIT_CTRL=1
 
 # NOTE: Cria o "branch" escolhido e o coloca em sincronia com a sua contraparte remota. 
 # Toma outras ações caso o "branch" já exista de forma garantir integridade. By Questor
 f_choose_not_master_branch() {
-    f_branches_list
+
+    # NOTE: Essa condicionals garante que a lista de "branches" seja obtida apenas
+    # uma vez para evitar falhas quando fazemos múltiplos "merges" seguidos. By Questor
+    if [ ${BM_CIRCUIT_CTRL} -eq 1 ] ; then
+        f_branches_list
+    fi
+
     f_div_section
     f_get_usr_input_mult "Choose the name of the branch that will be merged with the \"master\" branch.
-WARNING: The \"master\" branch will be OVERWRITTEN with these changes!" BRANCHES_OPT_ARR[@]
+WARNING: The \"master\" branch will be OVERWRITTEN with these changes!
+(Select \"exit\" to finish this process)" BRANCHES_OPT_ARR[@]
     NOT_MASTER_BRANCH="$GET_USR_INPUT_MULT_V_R"
+    if [[ $NOT_MASTER_BRANCH == "exit" ]] ; then
+        f_okay_exit
+    fi
+
+    # NOTE: Remove o branch da lista para evitar que seja feito novamente "merge"
+    # e evitar erros no processo. By Questor
+    # [Ref.: https://tecadmin.net/working-with-array-bash-script/ ]
+    unset BRANCHES_OPT_ARR[$GET_USR_INPUT_MULT_R+$GET_USR_INPUT_MULT_R+1]
+    unset BRANCHES_OPT_ARR[$GET_USR_INPUT_MULT_R+$GET_USR_INPUT_MULT_R]
+
+    # NOTE: Devido a limitações do bash índices do array fica comprometidos uma 
+    # vez que os valores não são de fato retirados, mas modificados para null. By Questor
+    # [Ref.: https://stackoverflow.com/a/47798640/3223785 ]
+    BRANCHES_OPT_ARR=(${BRANCHES_OPT_ARR[@]})
+
+    A_LENGTH=${#BRANCHES_OPT_ARR[*]}
+    for (( i=$[GET_USR_INPUT_MULT_R+GET_USR_INPUT_MULT_R]; i<=$(( $A_LENGTH -1 )); i=$[i+2] )) ; do
+        BRANCHES_OPT_ARR[$i]=$[${BRANCHES_OPT_ARR[$i]}-1]
+    done
+
     if [[ -z $(git show-ref refs/heads/$NOT_MASTER_BRANCH) ]]; then
 
         # https://www.reddit.com/r/git/comments/8wshx0/cant_get_git_checkout_t_originfeature_to_work/e1y1iu3?utm_source=share&utm_medium=web2x
@@ -380,11 +466,15 @@ WARNING: CHANGES ON LEFT WILL BE IGNORED!"
     # NOTE: Kill meld instance with diffs. By Questor
     kill -- -$DIFFTOOL_PID
 
-    # # NOTE: Kill diff window. By Questor
-    # pkill -f "meld"
+    # NOTE: Para encerrar o meld com as "diffs". Não tá muito legal, mas foi o único
+    # método até agora que garantiu encerrar o "meld" com as "diffs". By Questor
+    pkill -f "meld"
 
-    # NOTE: Remove a cópia do repositório no branch setado. By Questor
+    # NOTE: Remove a cópia do repositório no "branch" setado e a cópia utilizada
+    # para demonstrar as mudanças feitas ("diffs") nesse mesmo "branch" desde quando
+    # o mesmo foi separado do master. By Questor
     rm -rf "$(pwd)_master"
+    rm -rf "$(pwd)_not_master_diffs"
 
     # NOTE: Merge do branch "não master" no branch "master". O branch "master" será 
     # sobreescrito. By Questor
@@ -465,21 +555,7 @@ f_repo_fecth_all
 # >>>>> checkout "master"
 f_handle_master
 
-# >>>>> checkout "not_master"
-f_choose_not_master_branch
-
-f_master_to_not_master_diffs
-
-f_merge_master_n_not_master
-
-# >>>>> checkout "master"
-f_merge_not_master_n_master
-
-f_delete_merged_not_master
-
-f_git_push_changes "master"
-
-f_repo_backup 0
+f_branches_merge_circuit
 
 # EXEC <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
